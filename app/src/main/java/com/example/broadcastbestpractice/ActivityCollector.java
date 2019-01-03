7138d0633e74488e1cdd29d2f27ea2260c487926
wcg <wcg5219@sina.com>
2019-01-03 10:22:43 +0800

    Third Commit

---
+++ app/src/main/java/com/example/broadcastbestpractice/ActivityCollector.java
@@ -0,0 +1,30 @@
+package com.example.broadcastbestpractice;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 王春光 on 2018-12-18.
+ */
+
+public class ActivityCollector {
+    public static List<Activity> activities = new ArrayList<>();
+    public static void addActivity(Activity activity){
+        activities.add(activity);
+    }
+
+    public static void removeActivity(Activity activity) {
+        activities.remove(activity);
+    }
+
+    public static void finishAll() {
+        for (Activity activity : activities) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+    }
+
+}
